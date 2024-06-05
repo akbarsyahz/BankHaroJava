0bfd8ec0cf5a2bc5ae4d774701e7fa17257d3c93
akbarsyahz <akbarsyah0120@gmail.com>
2024-06-05 19:51:09 +0700

    SOLID PRINCIPLE!!!

--- src/com/bank_haro/dashboard/logic/IRegister.java
+++ src/com/bank_haro/dashboard/logic/IRegister.java
@@ -1,5 +1,5 @@
 package com.bank_haro.dashboard.logic;
 
 public interface IRegister {
-    public void register();
+    public void register(String username, String password, String address, Integer nik);
 }

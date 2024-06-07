138ef3c1098a3f6cbc692ad14f5feb5f1756299b
akbarsyahz <akbarsyah0120@gmail.com>
2024-06-07 09:36:22 +0700

    Pushhhhhh!!!!!

--- src/com/bank_haro/transaction/TransactionPage.java
+++ src/com/bank_haro/transaction/TransactionPage.java
@@ -8,50 +8,31 @@ import java.awt.event.ActionListener;
 public class TransactionPage implements ActionListener {
 
     JFrame frame = new JFrame();
-    JLabel titleLabel = new JLabel("BANK HARO");
-    JButton loginButton = new JButton("Login");
-    JButton registerButton = new JButton("Register");
-    JButton resetButton = new JButton("Reset");
-    JTextField userIDField = new JTextField();
-    JPasswordField userPasswordField = new JPasswordField();
-    JLabel userIDLabel = new JLabel("userID:");
-    JLabel userPasswordLabel = new JLabel("password:");
-    JLabel messageLabel = new JLabel();
+    JLabel titleLabel = new JLabel("PILIH MENU");
+    JButton withdrawButton = new JButton("WITHDRAW");
+    JButton depositButton = new JButton("DEPOSIT");
+    JButton earlierTransactionButton = new JButton("Earlier Transaction");
 
     public TransactionPage(){
         titleLabel.setBounds(155, 30, 200, 25);
         titleLabel.setFont(new Font(null,Font.BOLD,25));
 
-        userIDLabel.setBounds(50,100,75,25);
-        userPasswordLabel.setBounds(50,150,75,25);
+        withdrawButton.setBounds(125,200,100,25);
+        withdrawButton.setFocusable(false);
+        withdrawButton.addActionListener(this);
 
-        messageLabel.setBounds(125,250,250,35);
-        messageLabel.setFont(new Font(null,Font.ITALIC,25));
+        depositButton.setBounds(177,250,100,25);
+        depositButton.setFocusable(false);
+        depositButton.addActionListener(this);
 
-        userIDField.setBounds(125,100,200,25);
-        userPasswordField.setBounds(125,150,200,25);
-
-        loginButton.setBounds(125,200,100,25);
-        loginButton.setFocusable(false);
-        loginButton.addActionListener(this);
-
-        registerButton.setBounds(177,250,100,25);
-        registerButton.setFocusable(false);
-        registerButton.addActionListener(this);
-
-        resetButton.setBounds(225,200,100,25);
-        resetButton.setFocusable(false);
-        resetButton.addActionListener(this);
+        earlierTransactionButton.setBounds(177,250,100,25);
+        earlierTransactionButton.setFocusable(false);
+        earlierTransactionButton.addActionListener(this);
 
         frame.add(titleLabel);
-        frame.add(userIDLabel);
-        frame.add(userPasswordLabel);
-        frame.add(messageLabel);
-        frame.add(userIDField);
-        frame.add(userPasswordField);
-        frame.add(loginButton);
-        frame.add(resetButton);
-        frame.add(registerButton);
+        frame.add(withdrawButton);
+        frame.add(depositButton);
+        frame.add(earlierTransactionButton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLocationRelativeTo(null);
